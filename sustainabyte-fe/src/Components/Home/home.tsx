2dda472c4fe8a2747029022cd9695f8ea9596a1b
seNNNz0rel <67145369+seNNNz0rel@users.noreply.github.com>
2023-11-18 16:09:49 +0200

    WEB-4: added theming and prepared structure for feed page

--- sustainabyte-fe/src/Components/Home/home.tsx
+++ sustainabyte-fe/src/Components/Home/home.tsx
@@ -1,8 +1,90 @@
 import './home.scss'
-const Homepage = () => {
+import Container from "@mui/material/Container";
+import {useAuth} from "../Login/login";
+import {useTheme} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+const PostsFeed = () => {
+    // TODO: add posts inside container
+    return (
+        <Container
+            component="div"
+            sx={{
+                width: "60%",
+                alignItems: "center"
+            }}
+        >
+            <p>posts feed</p>
+        </Container>
+    )
+}
+
+const RegisterFeed = () => {
+    // @ts-ignore
+    const { authKey } = useAuth()
+    const theme = useTheme()
+
+    // check if user is not connected
+    if (!authKey) {
+        return (
+            <Container
+                component="div"
+                sx={{
+                    width: "20%",
+                }}
+            >
+                <Box>
+                    <h2>New to SustainaByte?</h2>
+                    <Button
+                        color={"inherit"}
+                    >
+                        Register
+                    </Button>
+                    <Button
+                        color={"inherit"}
+                    >
+                        Log in
+                    </Button>
+                </Box>
+            </Container>
+        )
+    }
+
     return (
         <>
         </>
+    )
+}
+
+const LeftFeed = () => {
+    return (
+        <Container
+            component="div"
+            sx={{
+                width: "20%"
+            }}
+        >
+            <p>left feed</p>
+        </Container>
+    )
+}
+
+const Homepage = () => {
+    const theme = useTheme()
+
+    return (
+        <Container
+            component="div"
+            sx={{
+                display: "flex",
+                color: `${theme.palette.text.primary}`
+            }}
+        >
+            <LeftFeed />
+            <PostsFeed />
+            <RegisterFeed />
+        </Container>
     )
 }
 
--- sustainabyte-fe/src/Components/PageLayout/pageLayout.tsx
+++ sustainabyte-fe/src/Components/PageLayout/pageLayout.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/header";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import {useTheme} from "@mui/material";
 
 const PageLayout = () => {
+    const theme = useTheme()
+
     return (
-        <Container component={"main"}>
+        <Box
+            component={"main"}
+            sx={{
+                width: '100%',
+                height: '100vh',
+                backgroundColor: `${theme.palette.background.default}`
+            }}
+        >
             <Header />
             <Outlet />
-        </Container>
+        </Box>
     )
 }
 
