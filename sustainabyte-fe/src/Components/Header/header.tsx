41399696ad20ac1a5dd2b2a2d67ba1937dbec385
NitaStefan <100165899+NitaStefan@users.noreply.github.com>
2023-11-18 20:03:13 +0200

    header

--- sustainabyte-fe/src/Components/Header/header.tsx
+++ sustainabyte-fe/src/Components/Header/header.tsx
@@ -1,32 +1,46 @@
+import { IconButton, TextField, useTheme} from '@mui/material';
+import WidgetsOutlinedIcon from '@mui/icons-material/WidgetsOutlined';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChatIcon from '@mui/icons-material/Chat';
 import './header.scss'
-import Button from "@mui/material/Button";
+// @ts-ignore
+import logo from '../../images/logo.png'
 
 const Header = () => {
+    const theme = useTheme()
     return (
         <header>
             <div className='header-container'>
-                <h2></h2>
                 <ul className='header-list'>
-                    <Button>
-                        <a href="#">
-                            Item
-                        </a>
-                    </Button>
-                    <Button>
-                        <a href="#">
-                            Item
-                        </a>
-                    </Button>
-                    <Button>
-                        <a href="#">
-                            Item
-                        </a>
-                    </Button>
-                    <Button>
-                        <a href="#">
-                            Item
-                        </a>
-                    </Button>
+                    <img src={logo} alt="logo"/>
+                    <TextField sx={{
+                    '& .MuiInputLabel-root': {
+                        color: `${theme.palette.text.primary}`, // Search text color
+                    },
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                        borderColor: `${theme.palette.text.primary}`, //Border unfocused
+                        },
+                    '&.Mui-focused fieldset': {
+                        borderColor: `${theme.palette.text.primary}`, // Border color when focused
+                        }
+                    },
+                    }} id="outlined-basic" label="Search" variant="outlined"/>
+                </ul>
+                <ul className='header-list'>
+                    <IconButton aria-label="menu" sx={{color: `${theme.palette.text.primary}`}}>
+                        <WidgetsOutlinedIcon />
+                    </IconButton>
+                    <IconButton aria-label="chat" sx={{color: `${theme.palette.text.primary}`}}>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton aria-label="notifications" sx={{color: `${theme.palette.text.primary}`}}>
+                        <NotificationsNoneIcon/>
+                    </IconButton>
+                    <IconButton aria-label="profile" sx={{color: `${theme.palette.text.primary}`}}>
+                        <AccountCircleIcon/>
+                    </IconButton>
                 </ul>
             </div>
         </header>
