bdd04eba040e90348f3da91af4639f00a55a4686
seNNNz0rel <67145369+seNNNz0rel@users.noreply.github.com>
2023-11-18 20:59:25 +0200

    WEB-4: arranged code

--- sustainabyte-fe/src/Components/Posts/posts.tsx
+++ sustainabyte-fe/src/Components/Posts/posts.tsx
@@ -1,11 +1,11 @@
 export interface Post { 
-        "title": "string",
-        "content": "string",
-        "creatorId": "string",
+        "title": string,
+        "content": string,
+        "creatorId": string,
         "kudos": 0,
-        "mediaURL": ["string"],
-        "createdAt": "2023-11-18T14:33:24.280Z",
-        "updatedAt": "2023-11-18T14:33:24.280Z",
+        "mediaUrl": string[],
+        "createdAt": string,
+        "updatedAt": string,
         "_id": "string",
         "__v": 0
     }
